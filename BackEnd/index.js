d44efa4e2c0345eb70dda29db932d5531ff998a5
Panchalparth471 <panchalparth471@gmail.com>
2025-01-17 14:21:28 +0530

    DONE

---
+++ BackEnd/index.js
@@ -0,0 +1,33 @@
+const express = require("express")
+const dotenv = require("dotenv")
+dotenv.config()
+dotenv.connect()
+
+app.use(express.json())
+app.use(
+	cors({
+		origin: "*",
+		credentials: true,
+	})
+);
+app.use(
+	fileUpload({
+		useTempFiles: true,
+		tempFileDir: "/tmp/",
+	})
+);
+
+
+app.get("/", (req, res) => {
+    return res.json({
+        success: true,
+        message:"Server Started Successfully",
+    })
+})
+
+app.listen(PORT, () => {
+    return res.json({
+        success: true,
+        message:"Port 4000 Running"
+    })
+})
